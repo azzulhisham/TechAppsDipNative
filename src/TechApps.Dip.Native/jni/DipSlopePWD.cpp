1dae0efb743583c5a0632389bd5a5d2554fa9446
azzulhisham <az_zulhisham@live.com>
2021-12-03 12:43:05 +0800

    completed stage 1 - Convolution

--- src/TechApps.Dip.Native/jni/DipSlopePWD.cpp
+++ src/TechApps.Dip.Native/jni/DipSlopePWD.cpp
@@ -8,20 +8,133 @@ int main() {
 
     DIP dip;
 
-	// vector<vector<int>> c_array = {{ 1, 2, 3, 4, 10 },
-	//                                { 5, 6, 7, 8, 50 },
-	//                                { 9, 10, 11, 12, 90 },
-	//                                { 13, 14, 15, 16, 130 }};
+	vector<vector<int>> inputMatrix = {{ -1, 2, 3, -4, 5, -6, 7, 8, 9, -10, 11 },
+                                   { -12, 13, 14, -15, 16, -16, 18, 19, 20, -21, 22 },
+                                   { -22, 23, 24, -25, 26, -26, 28, 29, 30, -31, 32 },
+                                   { -32, 33, 34, -35, 36, -36, 38, 39, 40, -41, 42 },
+                                   { -32, 33, 34, -35, 36, -36, 38, 39, 40, -41, 42 },
+                                   { -52, 53, 54, -55, 56, -56, 58, 59, 60, -61, 22 },
+            };
 
-    // int colSize = (int)(c_array[0].size());
-    // int rowSize = (int)(c_array.size());                               
+    vector<vector<MKL_Complex8>> c_array(inputMatrix.size(), vector<MKL_Complex8>(inputMatrix[0].size()));
+    for (int i = 0; i < c_array.size(); i++) {
+        for (int j = 0; j < c_array[0].size(); j++) {
+            c_array[i][j].real = (float)inputMatrix[i][j];
+        }
+    }
+                             
+    vector<vector<MKL_Complex8>> kernel = dip.CustGaussian2D(dip.windowX, dip.windowZ, 0);
+    vector<vector<MKL_Complex8>> kernelWin = dip.GenerateKernel(kernel, (int)(inputMatrix.size()), (int)(inputMatrix[0].size()));
 
-	// dip.PaddingArray(c_array, colSize, rowSize, 1, 1, false);
+    vector<MKL_Complex8> kernelWinArray(kernelWin.size() * kernelWin[0].size());
+    int temp = 0;
+
+    for(int r=0; r<kernelWin.size(); r++){
+        for(int c=0; c<kernelWin[0].size(); c++){
+            kernelWinArray[temp] = kernelWin[r][c];
+            temp += 1;
+        }
+    }
+
+    cout << "Kernel win : row=" << c_array.size() << " x col=" << c_array[0].size() << endl;
+
+
+    vector<vector<MKL_Complex8>> result = dip.Convolution(kernelWinArray, c_array); 
+	//vector<vector<MKL_Complex8>> result = dip.PaddingArray(c_array, 11, 6, 4, 4, false);
+
+    for (int r = 0; r < result.size(); r++)
+    {
+        for (int c = 0; c < result[0].size(); c++)
+        {
+            cout << result[r][c].real << "\t";
+        }
+        cout << endl;
+    }    
+
 
 	return 0;
 }
 
-vector<vector<int>> DIP::PaddingArray(vector<vector<int>> input, int colSize, int rowSize, int winX, int winZ,  bool circ) {
+vector<vector<MKL_Complex8>> DIP::Convolution(const vector<MKL_Complex8>& kernelWin, vector<vector<MKL_Complex8>> input) {
+
+    DIP dip;
+    
+    int colSize = (int)(input[0].size());
+    int rowSize = (int)(input.size());
+    vector<vector<MKL_Complex8>> padded = dip.PaddingArray(input, rowSize, colSize, dip.GetSizeKernel(dip.windowZ), dip.GetSizeKernel(dip.windowX), false);
+
+    //convert matrix format into data array
+    vector<MKL_Complex8> paddedArray(padded.size() * padded[0].size());
+    vector<MKL_Complex8> conv(padded.size() * padded[0].size());
+    int temp = 0;
+
+    for(int r=0; r<padded.size(); r++){
+        for(int c=0; c<padded[0].size(); c++){
+            paddedArray[temp] = padded[r][c];
+            temp += 1;
+        }
+    }
+
+
+	auto kn = paddedArray.size();
+    long padColSize = (long)(padded[0].size());
+    long padRowSize = (long)(padded.size());
+
+    //Fourier Forward - MatLab
+	MKL_LONG fft[2] { padRowSize, padColSize };
+	MKL_LONG status;
+	DFTI_DESCRIPTOR_HANDLE fh = NULL;
+
+	status = DftiCreateDescriptor(&fh, DFTI_SINGLE, DFTI_COMPLEX, 2, fft);
+	status = DftiCommitDescriptor(fh);
+	status = DftiComputeForward(fh, paddedArray.data()); // forward FFT
+	status = DftiFreeDescriptor(&fh);
+
+	// pointwise multiple kernel x inputMatrix (x + yi)*(u + vi) = (xu - yv) + (xv + yu)i
+	for (size_t i = 0; i < kn; i++) {
+		conv[i].real = (kernelWin[i].real * paddedArray[i].real) - (kernelWin[i].imag * paddedArray[i].imag);
+		conv[i].imag = (kernelWin[i].real * paddedArray[i].imag) + (kernelWin[i].imag * paddedArray[i].real);
+	}
+
+    //Fourier Backward - MatLab
+	DFTI_DESCRIPTOR_HANDLE bh = NULL;
+	status = DftiCreateDescriptor(&bh, DFTI_SINGLE, DFTI_COMPLEX, 2, fft);
+	status = DftiSetValue(bh, DFTI_BACKWARD_SCALE, 1.0f / kn);
+	status = DftiCommitDescriptor(bh);
+	status = DftiComputeBackward(bh, conv.data()); // backward FFT
+	status = DftiFreeDescriptor(&bh);
+
+    //convert data array into matrix after backward computation
+    vector<vector<MKL_Complex8>> resultConv(padded.size());
+    for(auto i=0; i<padded.size(); i++){
+        resultConv[i] = vector<MKL_Complex8>(padded[0].size());
+    }
+
+    temp = 0;
+
+    for(int r=0; r<padded.size(); r++){
+        for(int c=0; c<padded[0].size(); c++){
+            resultConv[r][c] = conv[temp];
+            temp += 1;
+        }
+    }
+
+    //return actual data size based on input data size
+    vector<vector<MKL_Complex8>> result(rowSize);
+    for(auto i=0; i<rowSize; i++){
+        result[i] = vector<MKL_Complex8>(colSize);
+    }
+
+    for(int r=0; r<rowSize; r++){
+        for(int c=0; c<colSize; c++){
+            result[r][c] =  resultConv[r][c];
+        }
+    }
+
+    return result;
+}
+
+vector<vector<MKL_Complex8>> DIP::PaddingArray(vector<vector<MKL_Complex8>> input, int rowSize, int colSize, int winX, int winZ,  bool circ) {
 
     int xMmid = (int)floor(winX / 2.0);
     int yMmid = (int)floor(winZ / 2.0);
@@ -29,11 +142,12 @@ vector<vector<int>> DIP::PaddingArray(vector<vector<int>> input, int colSize, in
     colSize += xMmid;
     rowSize += yMmid;
 
-    vector<vector<int>> extended = ExtendArray(input, colSize, rowSize);
+    vector<vector<MKL_Complex8>> extended = ExtendArray(input, rowSize, colSize);
 
     if (!circ) {
         return extended;
     }
+
 
     int mcMx = xMmid;
     int mcMy = yMmid;
@@ -41,17 +155,17 @@ vector<vector<int>> DIP::PaddingArray(vector<vector<int>> input, int colSize, in
     int meMx = mcMx + colSize - 1;
     int meMy = mcMy + rowSize - 1;
 
-    vector<vector<int>> newKernal = ExIndex(extended, mcMx, meMx - 1, mcMy, meMy - 1);
+    vector<vector<MKL_Complex8>> newKernal = ExIndex(extended, mcMx, meMx - 1, mcMy, meMy - 1);
 
     return newKernal;
     
 }
 
-vector<vector<int>> DIP::ExtendArray(vector<vector<int>> raw, int col, int row) {
+vector<vector<MKL_Complex8>> DIP::ExtendArray(vector<vector<MKL_Complex8>> raw, int row, int col) {
 
-    vector<vector<int>> expanded(row);
+    vector<vector<MKL_Complex8>> expanded(row);
     for(auto i=0; i<row; i++){
-        expanded[i] = vector<int>(col);
+        expanded[i] = vector<MKL_Complex8>(col);
     }
 
     for (int i = 0; i < row; i++)
@@ -69,11 +183,11 @@ vector<vector<int>> DIP::ExtendArray(vector<vector<int>> raw, int col, int row) 
 
 }
 
-vector<vector<int>> DIP::ExIndex(vector<vector<int>> m, int mcXFrom, int mcXTo, int mcYFrom, int mcYTo){
+vector<vector<MKL_Complex8>> DIP::ExIndex(vector<vector<MKL_Complex8>> m, int mcXFrom, int mcXTo, int mcYFrom, int mcYTo){
 
-    vector<vector<int>> newMatrix(m.size());
+    vector<vector<MKL_Complex8>> newMatrix(m.size());
     for(auto i=0; i<m.size(); i++){
-        newMatrix[i] = vector<int>(m[0].size());
+        newMatrix[i] = vector<MKL_Complex8>(m[0].size());
     }    
 
     for (int i = 0, x=mcYFrom; i < m.size(); x++, i++)  //row
@@ -86,14 +200,141 @@ vector<vector<int>> DIP::ExIndex(vector<vector<int>> m, int mcXFrom, int mcXTo, 
             if (y == m[0].size())
                 y = 0;
 
-            newMatrix[i][j] = m[i][j];
+            newMatrix[i][j] = m[x][y];
         }
     }
 
     return newMatrix;
 }
 
+vector<vector<MKL_Complex8>> DIP::GenerateKernel(vector<vector<MKL_Complex8>> raw, int width, int height) {
 
+    vector<vector<MKL_Complex8>> expandRaw = PaddingArray(raw, width, height, (int)(raw.size()), (int)(raw[0].size()), true);
 
+    vector<MKL_Complex8> paddedArray(expandRaw.size() * expandRaw[0].size());
+    vector<MKL_Complex8> kernel(expandRaw.size() * expandRaw[0].size());
+    int temp = 0;
+
+    for(int r=0; r<expandRaw.size(); r++){
+        for(int c=0; c<expandRaw[0].size(); c++){
+            paddedArray[temp] = expandRaw[r][c];
+            temp += 1;
+        }
+    }
+
+	auto kn = paddedArray.size();
+    long padColSize = (long)(expandRaw[0].size());
+    long padRowSize = (long)(expandRaw.size());
+
+	MKL_LONG fft[2] { padRowSize, padColSize };
+	MKL_LONG status;
+	DFTI_DESCRIPTOR_HANDLE fh = NULL;
+
+	status = DftiCreateDescriptor(&fh, DFTI_SINGLE, DFTI_COMPLEX, 2, fft);
+	status = DftiCommitDescriptor(fh);
+	status = DftiComputeForward(fh, paddedArray.data()); // forward FFT
+	status = DftiFreeDescriptor(&fh);
+
+    vector<vector<MKL_Complex8>> result(expandRaw.size());
+    for(auto i=0; i<expandRaw.size(); i++){
+        result[i] = vector<MKL_Complex8>(expandRaw[0].size());
+    }
+
+    temp = 0;
+
+    for(int r=0; r<expandRaw.size(); r++){
+        for(int c=0; c<expandRaw[0].size(); c++){
+            result[r][c] = paddedArray[temp];
+            temp += 1;
+        }
+    }
+
+    return result;
+
+}
+
+int DIP::GetSizeKernel(int size) {
+
+    return (2 * size) + 1;
+
+}
+
+float DIP::rotgauss(int x, int y, double theta, double sigmax, double sigmay) {
+
+    theta = (theta / 180) * M_PI;
+    double xm = (x) * cos(theta) - (y) * sin(theta);
+    double ym = (x) * sin(theta) + (y) * cos(theta);
+    double u = pow((xm / sigmax), 2) + pow((ym / sigmay), 2);
+    return (float)exp((-1.0 * u) / 2.0);
+
+}
+
+vector<vector<MKL_Complex8>> DIP::CustGaussian2D(int winX, int winZ, double theta) {
+
+            int x = GetSizeKernel(winX);
+            int y = GetSizeKernel(winZ);
+
+            vector<vector<float>> p(y, vector<float>(x));
+
+            float sumofgauss = 0.0f;
+            int rbegin = -1 * (int)round(x / 2.0);
+            int cbegin = -1 * (int)round(y / 2.0);
+
+            for (int r = 0; r < x; r++)
+            {
+                for (int c = 0; c < y; c++)
+                {
+                    p[c][r] = rotgauss(rbegin + (r + 1), cbegin + (c + 1), theta, winX, winZ);
+                    sumofgauss += p[c][r];
+                }
+            }
+
+            vector<vector<MKL_Complex8>> kernel(p.size(), vector<MKL_Complex8>(p[0].size()));
+            
+            for (int i = 0; i < p.size(); i++)
+            {
+                for (int j = 0; j < p[0].size(); j++)
+                {
+                    kernel[i][j].real = p[i][j] / sumofgauss;
+                }
+            }
+
+            return kernel;
+
+}
+
+vector<vector<float>> DIP::CustGaussianDouble2D(int winX, int winZ, double theta) {
+
+            int x = GetSizeKernel(winX);
+            int y = GetSizeKernel(winZ);
+
+            vector<vector<float>> p(y, vector<float>(x));
+
+            float sumofgauss = 0.0f;
+            int rbegin = -1 * (int)round(x / 2.0);
+            int cbegin = -1 * (int)round(y / 2.0);
+
+            for (int r = 0; r < x; r++)
+            {
+                for (int c = 0; c < y; c++)
+                {
+                    p[c][r] = rotgauss(rbegin + (r + 1), cbegin + (c + 1), theta, winX, winZ);
+                    sumofgauss += p[c][r];
+                }
+            }
+
+            vector<vector<float>> kernel(p.size(), vector<float>(p[0].size()));
+            
+            for (int i = 0; i < p.size(); i++)
+            {
+                for (int j = 0; j < p[0].size(); j++)
+                {
+                    kernel[i][j] = p[i][j] / sumofgauss;
+                }
+            }
+
+            return kernel;
+
+}
 
 
--- src/TechApps.Dip.Native/jni/dip.h
+++ src/TechApps.Dip.Native/jni/dip.h
@@ -1,17 +1,33 @@
 #include <mkl.h>
+#define _USE_MATH_DEFINES
+
 #include <mkl_dfti.h>
 #include <vector>
 #include <cmath>
 #include <limits>
 #include <iostream>
 #include <sstream>
+#include<cmath>
 
 using std::vector;
 
 class DIP
 {
 public:
-    vector<vector<int>> PaddingArray(vector<vector<int>> input, int colSize, int rowSize, int winX, int winZ,  bool circ);
-    vector<vector<int>> ExtendArray(vector<vector<int>> raw, int col, int row);  
-    vector<vector<int>> ExIndex(vector<vector<int>> m, int mcXFrom, int mcXTo, int mcYFrom, int mcYTo);  
+    int windowX = 2;
+    int windowZ = 2;
+    int dX = 1;
+    int dZ = 1;
+    float pSize = 0.0001f;
+
+    vector<vector<MKL_Complex8>> PaddingArray(vector<vector<MKL_Complex8>> input, int rowSize, int colSize, int winX, int winZ,  bool circ);
+    vector<vector<MKL_Complex8>> ExtendArray(vector<vector<MKL_Complex8>> raw, int row, int col);  
+    vector<vector<MKL_Complex8>> ExIndex(vector<vector<MKL_Complex8>> m, int mcXFrom, int mcXTo, int mcYFrom, int mcYTo); 
+    vector<vector<MKL_Complex8>> Convolution(const vector<MKL_Complex8>& kernelWin, vector<vector<MKL_Complex8>> input); 
+
+    int GetSizeKernel(int size);
+    float rotgauss(int x, int y, double theta, double sigmax, double sigmay);
+    vector<vector<MKL_Complex8>> GenerateKernel(vector<vector<MKL_Complex8>> raw, int width, int height);
+    vector<vector<MKL_Complex8>> CustGaussian2D(int winX, int winZ, double theta);
+    vector<vector<float>> CustGaussianDouble2D(int winX, int winZ, double theta);
 };
